9b62ccb70572580e03187d706ed2eb0cc10fe92a
Elijah <ekollie324@gmail.com>
2023-10-31 09:38:37 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+
+let num1 = 7.75
+let num2 = 8
+let num3 = 20
+let num4 = 8
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 100)
+
+const mod = num3 % num4
+
+const max = Math.max(num1, num2, num3, num4)
